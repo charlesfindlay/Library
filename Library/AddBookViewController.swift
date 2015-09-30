ffce3550479a264240640447ada30f6cc8e3be85
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-09-30 16:31:03 -0400

    Basic addBook working with title

--- Library/AddBookViewController.swift
+++ Library/AddBookViewController.swift
@@ -10,4 +10,21 @@ import UIKit
 
 class AddBookViewController : UIViewController {
     
+    
+    @IBOutlet weak var newTitle: UITextField!
+    var workingLibrary = Library()
+    
+    
+    @IBAction func getNewBook(sender: AnyObject) {
+        var newBook = Book()
+        newBook.title = newTitle.text
+        workingLibrary.books.append(newBook)
+        
+        //Dismiss modal view
+        self.dismissViewControllerAnimated(true) {
+            
+        }
+    }
+    
+    
 }
--- Library/HomeViewController.swift
+++ Library/HomeViewController.swift
@@ -9,25 +9,37 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    var myLibrary = Library().seedLibrary()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var myLibrary = Library().seedLibrary()
-        print(myLibrary.name!)
-        for item in myLibrary.books {
-            print(item.title!)
-        }
         
+        //Debugging code
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-        
     }
 
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "addBook" {
+            let vc = segue.destinationViewController as! AddBookViewController
+            vc.workingLibrary = myLibrary
+        }
+    }
+    
+    
+    @IBAction func printBookTitles(sender: UIButton) {
+        print(myLibrary.name!)
+        for item in myLibrary.books {
+            print(item.title!)
+        }
+    }
+    
 
 }
 
