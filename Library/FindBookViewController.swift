84cbc0a0c3981ea5b9fc6f7fabbc8e33355d5b35
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-10-02 15:12:23 -0400

    Updated layout home page

---
+++ Library/FindBookViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FindBookViewController.swift
+//  Library
+//
+//  Created by Student on 10/2/15.
+//  Copyright © 2015 Charles Findlay. All rights reserved.
+//
+
+import UIKit
+
+class FindBookViewController : UIViewController {
+
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var titleToFind: UITextField!
+    
+    var isDelete = false
+    var isEdit = true
+    
+    override func viewDidAppear(animated: Bool) {
+        //Set code here to update label text to display either edit or delete.
+    }
+
+
+
+
+
+}
